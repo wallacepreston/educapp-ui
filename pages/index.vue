4893da33bac47c723785f3a52b4d9c795883c7ca
Preston Wallace <preston.wallace@liquidityservices.com>
2019-07-29 16:37:14 -0700

    initial commit. Working compiled files. Simple layout. No data yet.

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Educ-App
+        </v-card-title>
+        <v-card-text>
+          <p>This is an application meant to help tutors manage their students and class sessions.</p>
+          
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  }
+}
+</script>
---
+++ pages/users/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-card raised>
+      <v-card-title>
+
+      <h1>Users</h1>
+      </v-card-title>
+      <v-card-text>
+        <div v-for="user in users">
+          <p>Student Name: {{user.name}}</p>
+          <p>Age: {{user.age}}</p>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+    data () {
+      return {
+        users: [
+          { name: 'Sally', age: 10},
+          { name: 'Robert', age: 12},
+          { name: 'Bobby', age: 8},
+          { name: 'Les', age: 50},
+          { name: 'Ava', age: 3},
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
